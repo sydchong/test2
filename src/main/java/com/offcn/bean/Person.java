38f644db48b0e87385cfcfbe9a42f2472fc6069c
sydchong <wchongrock@163.com>
2018-09-05 15:03:52 +0800

    ddd

--- src/main/java/com/offcn/bean/Person.java
+++ src/main/java/com/offcn/bean/Person.java
@@ -3,5 +3,7 @@ package com.offcn.bean;
 public class Person {
  public static void main(String[] args) {
 	System.out.println("初始内容");
+	System.out.println("第一次修改");
+	System.out.println("第二次修改");
 }
 }
